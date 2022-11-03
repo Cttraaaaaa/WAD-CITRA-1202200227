0fe887d55d4b8d7ee91941162acc14b45b2e935e
Barajati12 <barajati@student.telkomuniversity.ac.id>
2022-11-03 14:12:10 +0700

    DEBUGGING 1

--- MODUL2 CITRA/citra_booking.php
+++ MODUL2 CITRA/citra_booking.php
@@ -1,118 +1,118 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>WAD CITRA 1202200227</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
-      crossorigin="anonymous"
-    />
-    <link
-      rel="stylesheet"
-      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-      crossorigin="anonymous"
-    />
-</head>
-  
-  <body>
-       <!-- navigasi -->
-       <nav
-      class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg fixed-top">
-      <div class="collapse navbar-collapse d-flex justify-content-center">
-        <button
-          class="navbar-toggler"
-          action="citra_home.php"
-          method="GET"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-        </button>
-            <div class="collapse navbar-collapse text" id="navbarText">
-                <ul class="navbar-nav ms-auto mb-3 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link" href="citra_home.php">Home</a>
-                    </li>                      
-                    <a class="nav-link" href="citra_booking.php">Booking</a>
-                    </li>
-        
-    </nav>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>WAD CITRA 1202200227</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+          crossorigin="anonymous"
+        />
+    </head>
+      
+      <body>
+          <!-- navigasi -->
+          <nav
+          class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg fixed-top">
+          <div class="collapse navbar-collapse d-flex justify-content-center">
+            <button
+              class="navbar-toggler"
+              action="citra_home.php"
+              method="GET"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarText"
+              aria-controls="navbarText"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+            </button>
+                <div class="collapse navbar-collapse text" id="navbarText">
+                    <ul class="navbar-nav ms-auto mb-3 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link" href="citrahome.php">Home</a>
+                        </li>                      
+                        <a class="nav-link" href="citra_booking.php">Booking</a>
+                        </li>
+            
+        </nav>
 
-    <!-- Pendaftaran Mobil -->
-    <h2 class="display-3"></h2>
-    <div class="container-fluid pt-5 pb-5 bg-light">
-    <div class="container text-center">
-    <form action="citra_mybook" method="GET">
-    <h1 class="text-center">Rent Your Car Now!</h1>
-      <form action="getInput.php" method="GET">
+        <!-- Pendaftaran Mobil -->
+        <h2 class="display-3"></h2>
+        <div class="container-fluid pt-5 pb-5 bg-light">
+        <div class="container text-center">
+        <form action="citra_mybook" method="GET">
+        <h1 class="text-center">Rent Your Car Now!</h1>
+          <form action="getInput.php" method="GET">
 
-        <div class="mb-3">
-            <label for="Nama" class="form-label">Name</label>
-            <input type="text" class="form-control" id="Nama" name="name" placeholder= " Masukan Nama Lengkapmu">
-        </div>
+            <div class="mb-3">
+                <label for="Nama" class="form-label">Name</label>
+                <input type="text" class="form-control" id="Nama" name="name" placeholder= " Masukan Nama Lengkapmu">
+            </div>
 
-        <div class="mb-3">
-            <label for="nohp" class="form-label">Book Date</label>
-            <input type="date" class="form-control" id="bookdate" name="bookdate">
-        </div>
+            <div class="mb-3">
+                <label for="nohp" class="form-label">Book Date</label>
+                <input type="date" class="form-control" id="bookdate" name="bookdate">
+            </div>
 
-        <div class="mb-3">
-            <label for="Alamat" class="form-label">Start Time</label>
-            <input type="text" class="form-control" id="time" name="time">
-        </div>        
+            <div class="mb-3">
+                <label for="Alamat" class="form-label">Start Time</label>
+                <input type="text" class="form-control" id="time" name="time">
+            </div>        
 
-        <div class="mb-3">
-            <label for="Alamat" class="form-label">Phone Number</label>
-            <input type="text" class="form-control" id="Phone" name="Phone">
-        </div>        
+            <div class="mb-3">
+                <label for="Alamat" class="form-label">Phone Number</label>
+                <input type="text" class="form-control" id="Phone" name="Phone">
+            </div>        
 
-        <p>Car Type</p>
-        <select class="form-select" aria-label="Default select example">
-            <option selected>Honda HRV</option>
-            <option value="1">Toyota Raize</option>
-            <option value="2">Mitsubishi Pajero</option>
-        </select>
+            <p>Car Type</p>
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Honda HRV</option>
+                <option value="1">Toyota Raize</option>
+                <option value="2">Mitsubishi Pajero</option>
+            </select>
 
-          <br>
+              <br>
 
-          <p>Add Service(s)</p>
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-            Health protocol / Rp 25.000
-          </label>
-        </div>
+              <p>Add Service(s)</p>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                Health protocol / Rp 25.000
+              </label>
+            </div>
 
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-            <label class="form-check-label" for="flexCheckChecked">
-            Driver / Rp.100.000
-          </label>
-        </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                <label class="form-check-label" for="flexCheckChecked">
+                Driver / Rp.100.000
+              </label>
+            </div>
 
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-            <label class="form-check-label" for="flexCheckChecked">
-            Fuel filled / Rp.250.000
-            </label>
-        </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                <label class="form-check-label" for="flexCheckChecked">
+                Fuel filled / Rp.250.000
+                </label>
+            </div>
 
-          <br>
+              <br>
 
-        <div class="mb-3">
-            <div class="d-flex justify-content-center">
-            <form action="citra_mybook.php" method="GET">
-                    <button type="submit" class="btn btn-primary d-grid mx-auto" value="book" name="mybook">Book Now</button>
+            <div class="mb-3">
+                <div class="d-flex justify-content-center">
+                <form action="citra_mybook.php" method="GET">
+                        <button type="submit" class="btn btn-primary d-grid mx-auto" value="book" name="mybook">Book Now</button>
+                </form>
+            </div>
             </form>
-        </div>
-        </form>
-    <body>
-</html>
+        <body>
+    </html>
--- MODUL2 CITRA/citra_home.php
+++ MODUL2 CITRA/citra_home.php
@@ -1,141 +1,141 @@
-<!doctype html>
-<html lang="en">
- <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>WAD CITRA 1202200227</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
-      crossorigin="anonymous"
-    />
-    <link
-      rel="stylesheet"
-      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-      crossorigin="anonymous"
-    />
-  </head>
+      <!doctype html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>WAD CITRA 1202200227</title>
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+            integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+            crossorigin="anonymous"
+          />
+        </head>
 
-  <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+        <!-- JavaScript Bundle with Popper -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
-  <body>
-    <!-- navigasi -->
-    <nav
-      class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg fixed-top">
-      <div class="collapse navbar-collapse d-flex justify-content-center">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-        </button>
-        <div class="collapse navbar-collapse text" id="navbarText">
-          <ul class="navbar-nav ms-auto mb-3 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" href="citra_home.php">Home</a>
-            </li>              
-            <a class="nav-link" href="citra_booking.php">Booking</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-        <!-- banner -->
-        <h2 class="display-3" id="Home"></h2>
-        <br>
-        <br>
-        <br>     
-      <div class="container text-center">
-        <h2>WELCOME TO EAD RENT</h2>
-        <a> Find your best deal, here!<a>
-      </div>
-     <!-- Jenis mobil -->
-     <div class="container-fluid jenis pt-5 pb-5">
-      <div class="container text-center">
-        <div class="row pt-4 gx-4 gy-4">
-          <div class="col-md-4">
-            <div class="card crop-img">
-              <img
-                src="img/raize.jpeg"
-                class="card-img-top"
-                width="200"
-                height="200"
-              />
-              <div class="card-body">
-                <h5 class="card-title"> Toyota Raize </h5>
-                <p class="card-text-left">
-                  Mulai Dari IDK 900.000/ Hari
-                <ol class="list-group">
-                  <li class="list-group-item" style="color:blue">4 Kursi</li>
-                  <li class="list-group-item" style="color:blue"> 1200 cc</li>
-                  <li class="list-group-item" style="color:blue">Matic</li>
-                </ol> 
-                <br>
-                <form action="citra_booking.php" method="GET">
-                    <button type="submit" class="btn btn-primary d-grid mx-auto" value="Raize" name="booknow">Book Now</button>
-                </form>
-                </p>
+        <body>
+          <!-- navigasi -->
+          <nav
+            class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg fixed-top">
+            <div class="collapse navbar-collapse d-flex justify-content-center">
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarText"
+                aria-controls="navbarText"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+              </button>
+              <div class="collapse navbar-collapse text" id="navbarText">
+                <ul class="navbar-nav ms-auto mb-3 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" href="citra_home.php">Home</a>
+                  </li>              
+                  <a class="nav-link" href="citra_booking.php">Booking</a>
+                  </li>
+                </ul>
               </div>
             </div>
-          </div>
+          </nav>
+              <!-- banner -->
+              <h2 class="display-3" id="Home"></h2>
+              <br>
+              <br>
+              <br>     
+            <div class="container text-center">
+              <h2>WELCOME TO EAD RENT</h2>
+              <a> Find your best deal, here!<a>
+            </div>
+          <!-- Jenis mobil -->
+          <div class="container-fluid jenis pt-5 pb-5">
+            <div class="container text-center">
+              <div class="ow pt-4 gx-4 gy-4">
+                <div class="col-md-4">
+                  <div class="card crop-img">
+                    <img
+                      src="img/raize.jpeg"
+                      class="card-img-top"
+                      width="200"
+                      height="200"
+                    />
+                    <div class="card-body">
+                      <h5 class="card-title"> Toyota Raize </h5>
+                      <p class="card-text-left">
+                        Mulai Dari IDK 900.000/ Hari
+                      <ol class="list-group">
+                        <li class="list-group-item" style="color:blue">4 Kursi</li>
+                        <li class="list-group-item" style="color:blue"> 1200 cc</li>
+                        <li class="list-group-item" style="color:blue">Matic</li>
+                      </ol> 
+                      <br>
+                      <form action="citra_booking.php" method="GET">
+                          <button type="submit" class="btn btn-primary d-grid mx-auto" value="Raize" name="booknow">Book Now</button>
+                      </form>
+                      </p>
+                    </div>
+                  </div>
+                </div>
 
-          <div class="col-md-4">
-            <div class="card crop-img">
-              <img
-                src="img/pajero.jpeg"
-                class="card-img-top"
-                width="200"
-                height="200"
-              />
-              <div class="card-body">
-                <h5 class="card-title"> Mitsubishi Pajero</h5>
-                <p class="card-text">
-                  Mulai Dari IDK 1000.000/ Hari
-                  <ol class="list-group"> 
-                    <li class="list-group-item" style="color:blue">5 Kursi</li>
-                    <li class="list-group-item" style="color:blue">2442 cc</li>
-                    <li class="list-group-item" style="color:blue">Matic</li>
-                  </ol>
-                  <br>
-                  <form action="citra_booking.php" method="GET">
-                    <button type="submit" class="btn btn-primary d-grid mx-auto" value="Pajero" name="booknow">Book Now</button>
-                </form>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card crop-img">
-              <img
-                src="img/HRV.png"
-                class="card-img-top"
-                width="200"
-                height="200"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Honda HRV</h5>
-                <p class="card-text">
-                  Mulai Dari IDK 900.000/ Hari
-                  <ol class="list-group ">
-                    <li class="list-group-item" style="color:blue">5 Kursi</li>
-                    <li class="list-group-item" style="color:blue">1498 cc</li>
-                    <li class="list-group-item" style="color:blue">Matic</li>
-                  </ol>
-                  <br>
-                <form action="citra_booking.php" method="GET">
-                    <button type="submit" class="btn btn-primary d-grid mx-auto" value="HRV" name="booknow">Book Now</button>
-                </form>
-                </p>
-              </div>
-            </div>
-          </div>
-  </body>
-</html>
+                <div class="col-md-4">
+                  <div class="card crop-img">
+                    <img
+                      src="img/pajero.jpeg"
+                      class="card-img-top"
+                      width="200"
+                      height="200"
+                    />
+                    <div class="card-body">
+                      <h5 class="card-title"> Mitsubishi Pajero</h5>
+                      <p class="card-text">
+                        Mulai Dari IDK 1000.000/ Hari
+                        <ol class="list-group"> 
+                          <li class="list-group-item" style="color:blue">5 Kursi</li>
+                          <li class="list-group-item" style="color:blue">2442 cc</li>
+                          <li class="list-group-item" style="color:blue">Matic</li>
+                        </ol>
+                        <br>
+                        <form action="citra_booking.php" method="GET">
+                          <button type="submit" class="btn btn-primary d-grid mx-auto" value="Pajero" name="booknow">Book Now</button>
+                      </form>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="card crop-img">
+                    <img
+                      src="img/HRV.png"
+                      class="card-img-top"
+                      width="200"
+                      height="200"
+                    />
+                    <div class="card-body">
+                      <h5 class="card-title">Honda HRV</h5>
+                      <p class="card-text">
+                        Mulai Dari IDK 900.000/ Hari
+                        <ol class="list-group ">
+                          <li class="list-group-item" style="color:blue">5 Kursi</li>
+                          <li class="list-group-item" style="color:blue">1498 cc</li>
+                          <li class="list-group-item" style="color:blue">Matic</li>
+                        </ol>
+                        <br>
+                      <form action="#citra_booking.php" method="GET">
+                          <button type="submit" class="btn btn-primary d-grid mx-auto" value="HRV" name="booknow">Book Now</button>
+                      </form>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+        </body>
+      </html>
